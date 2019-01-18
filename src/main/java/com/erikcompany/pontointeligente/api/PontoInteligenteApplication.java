271b05265170c168079e846ce258bde4557c3657
erikflima <erik.alves253@gmail.com>
2019-01-18 05:54:20 -0200

    Push para enviar arquivos para o GitHub.

--- src/main/java/com/erikcompany/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/erikcompany/pontointeligente/api/PontoInteligenteApplication.java
@@ -12,7 +12,7 @@ public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("\n\nErik - Executando o metodo main e apos isso o metodo SpringApplication.run, que roda a aplicacao literalmente!\n\n");
+		System.out.println("\n\nErik -  Executando o metodo main e apos isso o metodo SpringApplication.run, que roda a aplicacao literalmente!\n\n");
 
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 		
